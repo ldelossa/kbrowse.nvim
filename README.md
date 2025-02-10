4f470f84a21db595b5e3e080020291c7814650df
ldelossa <louis.delos@gmail.com>
2025-02-10 17:16:02 -0500

    initial impl

---
+++ README.md
@@ -0,0 +1,20 @@
+# KBrowse
+
+Create links to https://elixir.bootlin.com/linux/ from a local kernel source
+tree.
+
+Handy when browsing kernel trees downloaded directly from git.kernel.org
+
+## Usage
+
+Call the setup function from your favorite package manager:
+```
+	require("kbrowse").setup()
+```
+
+Two user commands are then installed.
+
+`KBrowse` - Opens a browser to the line(s) at the cursor.
+`KBrowseClipboard` - Copy the link created directly to your clipboard.
+
+Both commands will work in visual mode.
---
+++ lua/kbrowse/init.lua
@@ -0,0 +1,72 @@
+local M = {}
+
+local elixir_prefix = "https://elixir.bootlin.com/linux"
+
+local function get_kernel_version()
+	-- get output of git describe --tags --abbrev=0
+	local f = io.popen("git describe --tags --abbrev=0")
+	if f == nil then
+		return nil
+	end
+	local version = f:read("*a")
+	f:close()
+	-- strip any new lines from version
+	version = version:gsub("\n", "")
+	return version
+end
+
+local function get_repo_root()
+	-- git rev-parse --show-toplevel
+	local f = io.popen("git rev-parse --show-toplevel")
+	if f == nil then
+		return nil
+	end
+	local root = f:read("*a")
+	f:close()
+	-- strip any new lines from repo_root
+	root = root:gsub("\n", "")
+	return root
+end
+
+-- create a link for given attributers
+local function format_link(version, line_start, line_end, file)
+	local l = string.format("%s/%s/source%s#L%d", elixir_prefix, version, file, line_start)
+	if line_end ~= nil and tonumber(line_end) > tonumber(line_start) then
+		l = l .. '-L' .. line_end
+	end
+	return l
+end
+
+local function user_command_handler(opts)
+	local name = opts.name
+	local repo_root = get_repo_root() .. '/'
+	local buff_name = vim.api.nvim_buf_get_name(0)
+
+	local buff_name_relative = buff_name:sub(#repo_root)
+
+	local start_line = opts.line1
+	local end_line = opts.line2
+
+	local kversion = get_kernel_version()
+	local link = format_link(kversion, start_line, end_line, buff_name_relative)
+
+	if opts.name == "KBrowseClipboard" then
+		vim.fn.setreg("+", link)
+		vim.notify("Link copied to clipboard")
+		return
+	end
+
+	if vim.fn.has("mac") == 1 then
+		os.execute("open " .. link)
+	end
+	if vim.fn.has("unix") == 1 then
+		os.execute("xdg-open " .. link)
+	end
+end
+
+function M.setup()
+	vim.api.nvim_create_user_command("KBrowse", user_command_handler, { range = true })
+	vim.api.nvim_create_user_command("KBrowseClipboard", user_command_handler, { range = true })
+end
+
+return M
